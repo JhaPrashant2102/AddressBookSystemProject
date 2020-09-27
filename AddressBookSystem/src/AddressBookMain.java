0e461038a1c646a710cdcda66fcf0037b47c2b35
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-09-27 23:00:01 +0530

    UC3 added

--- AddressBookSystem/src/AddressBookMain.java
+++ AddressBookSystem/src/AddressBookMain.java
@@ -10,38 +10,64 @@ import java.util.*;
  */
 public class AddressBookMain {
 
-	public String firstName;
-	public String lastName;
-	public String city;
-	public String state;
-	public String phoneNumber;
-	public String emailId;
+	// list to store contacts
+	private LinkedList<Contact> bookList;
+	private Map<String, Contact> bookMap;
 
-	public AddressBookMain(String firstName, String lastName, String city, String state, String phoneNumber,
+	// constructor of addressbook
+	public AddressBookMain() {
+		bookList = new LinkedList<>();
+		// bookMap = new HashMap<>();
+	}
+
+	// function to add new contacts to the book
+	public void addNewContact(String firstName, String lastName, String city, String state, String phoneNumber,
 			String emailId) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.city = city;
-		this.state = state;
-		this.phoneNumber = phoneNumber;
-		this.emailId = emailId;
+		Contact contactX = new Contact(firstName, lastName, city, state, phoneNumber, emailId);
+		bookList.add(contactX);
+		// bookMap.put(first, value)
+	}
+
+	public void changeDetails() {
+		System.out.println("Enter the First Name of person whose details are to be changed :");
+		Scanner nsc = new Scanner(System.in);
+		String personName = nsc.nextLine();
+		for (int i = 0; i < bookList.size(); i++) {
+			if (bookList.get(i).firstName == personName) {
+				System.out.println("Enter new city :");
+				bookList.get(i).city = nsc.nextLine();
+				return;
+				// Similarly other changes can be made
+			}
+		}
+		System.out.println("No record found for this person");
 	}
 
 	public static void main(String args[]) {
 		System.out.println("Welcome to Address Book");
-		System.out.println("Enter First Name :");
+		System.out.println("Enter number of person in address Book");
 		Scanner sc = new Scanner(System.in);
-		String fName = sc.nextLine();
-		System.out.println("Enter Last Name :");
-		String lName = sc.nextLine();
-		System.out.println("Enter the City :");
-		String cit = sc.nextLine();
-		System.out.println("Enter the State :");
-		String sta = sc.nextLine();
-		System.out.println("Enter your Phone Number :");
-		String phoneNo = sc.nextLine();
-		System.out.println("Enter your Email Address :");
-		String email = sc.nextLine();
-		AddressBookMain newBoook = new AddressBookMain(fName,lName,cit,sta,phoneNo,email);
+		int n = sc.nextInt();
+		AddressBookMain book = new AddressBookMain();
+		sc.nextLine();
+		while (n > 0) {
+			n--;
+			System.out.println("Enter First Name :");
+			String firstName = sc.nextLine();
+			System.out.println("Enter Last Name :");
+			String lastName = sc.nextLine();
+			System.out.println("Enter the City :");
+			String city = sc.nextLine();
+			System.out.println("Enter the State :");
+			String state = sc.nextLine();
+			System.out.println("Enter your Phone Number :");
+			String phoneNumber = sc.nextLine();
+			System.out.println("Enter your Email Address :");
+			String emailId = sc.nextLine();
+			// AddressBookMain newBoook = new
+			// AddressBookMain(fName,lName,cit,sta,phoneNo,email);
+			book.addNewContact(firstName, lastName, city, state, phoneNumber, emailId);
+		}
+		book.changeDetails();
 	}
 }
---
+++ AddressBookSystem/src/Contact.java
@@ -0,0 +1,30 @@
+/**
+ * contact class for addressBook
+ */
+
+/**
+ * @author prashant
+ *
+ */
+public class Contact {
+
+	// variables
+	public String firstName;
+	public String lastName;
+	public String city;
+	public String state;
+	public String phoneNumber;
+	public String emailId;
+
+	//Constructor
+	public Contact(String firstName, String lastName, String city, String state, String phoneNumber, String emailId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.state = state;
+		this.phoneNumber = phoneNumber;
+		this.emailId = emailId;
+	}
+	
+	
+}
