b635669e3de169a2ecadbe7d088a6fbfcc39a572
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-09-27 20:29:33 +0530

    UC1 added

--- AddressBookSystem/src/AddressBookMain.java
+++ AddressBookSystem/src/AddressBookMain.java
@@ -7,6 +7,15 @@
  *
  */
 public class AddressBookMain {
+	
+	String firstName;
+	String lastName;
+	String city;
+	String state;
+	String phoneNumber;
+	String emailId;
+	
+	
 	public static void main(String args[]) {
 		System.out.println("Welcome to Address Book");
 	}
