8ab38049a2af8fadf1302b4046d9aeeb07e7a091
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-09-27 20:09:08 +0530

    Welcome message added

---
+++ AddressBookSystem/src/AddressBookMain.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+/**
+ * @author prashant
+ *
+ */
+public class AddressBookMain {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Address Book");
+	}
+}
